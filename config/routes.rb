c86f85669734fd38477bfade67a37bd1a964bbf3
鷲見和希 <sumi@sumikazukinoMacBook-Pro.local>
2020-04-07 13:56:16 +0900

    リソース名を変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: "admins#index"
-  resources :admins
+  namespace :admin do
+    resources :users
+  end
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
