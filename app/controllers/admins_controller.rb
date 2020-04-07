3d673e6e1df1f06c46fb48b0bff422e3c8c75e63
鷲見和希 <sumi@sumikazukinoMacBook-Pro.local>
2020-04-07 09:16:00 +0900

    インスタンス変数名を@adminから@admin_userに変更

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,19 +1,19 @@
 class AdminsController < ApplicationController
   def index
-    @admin = User.where(role: 1)
+    @admin_users = User.where(role: 1)
   end
 
   def show
-    @admin = User.find(params[:id])
+    @admin_user = User.find(params[:id])
   end
 
   def new
-    @admin = User.new
+    @admin_user = User.new
   end
 
   def create
-    @admin = User.new(admin_params)
-    if @admin.save
+    @admin_user = User.new(admin_params)
+    if @admin_user.save
       redirect_to admins_path, notice: "ユーザを登録しました"
     else
       flash.now[:danger] = "ユーザの登録に失敗しました"
@@ -22,12 +22,12 @@ class AdminsController < ApplicationController
   end
 
   def edit
-    @admin = User.find(params[:id])
+    @admin_user = User.find(params[:id])
   end
 
   def update
-    @admin = User.find(params[:id])
-    if @admin.update(admin_params)
+    @admin_user = User.find(params[:id])
+    if @admin_user.update(admin_params)
       redirect_to admin_path, notice: "ユーザの情報を更新しました"
     else
       flash.now[:danger] = "ユーザの情報更新に失敗しました"
@@ -36,8 +36,8 @@ class AdminsController < ApplicationController
   end
 
   def destroy
-    @admin = User.find(params[:id])
-    if @admin.destory
+    @admin_user = User.find(params[:id])
+    if @admin_user.destory
       redirect_to admins_path, notice: "ユーザの情報を削除しました"
     else
       flash.now[:alert] = "ユーザの情報削除に失敗しました"
