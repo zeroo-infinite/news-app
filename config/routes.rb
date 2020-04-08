455cbf94ee8fa04c061a29f17f402b92c2590b37
鷲見和希 <sumi@sumikazukinoMacBook-Pro.local>
2020-04-08 16:48:04 +0900

    記事詳細のURLを/pages/<任意の文字列>で表示

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resources :articles, except: [:index, :show]
   end
   resources :articles, only: :index
+  get "pages/:slug", to: "articles#show", as: :article
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
