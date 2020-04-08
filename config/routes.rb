6a72be7be9c78eb6f8456cfcdf9c7d9fc2254830
鷲見和希 <sumi@sumikazukinoMacBook-Pro.local>
2020-04-08 16:43:32 +0900

    記事一覧作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     resources :users
     resources :articles, except: [:index, :show]
   end
+  resources :articles, only: :index
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
