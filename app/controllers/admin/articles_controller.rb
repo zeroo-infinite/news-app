bd9985d3e6864f6199df346791a117cdaf8067a4
鷲見和希 <sumi@sumikazukinoMacBook-Pro.local>
2020-04-10 11:36:15 +0900

    adminのnamespaceをmoduleで表現

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -1,49 +1,49 @@
-class Admin::ArticlesController < ApplicationController
-  def new
-    @article = Article.new
-    @categories = Category.all
-  end
-
-  def create
-    @article = current_user.articles.build(article_params)
-    if @article.save
-      @article.create_article_categorization!(category_id: params[:article][:category_id]) if params[:article][:category_id].present?
-      redirect_to articles_path, notice: "記事を作成しました"
-    else
-      flash.now[:danger] = "記事の作成に失敗しました"
-      render :new
+module Admin
+  class ArticlesController < Admin::BaseController
+    def new
+      @article = Article.new
+      @categories = Category.all
     end
-  end
 
-  def edit
-    @article = Article.find(params[:id])
-    @categories = Category.all
-  end
-
-  def update
-    @article = Article.find(params[:id])
-    if @article.update(article_params)
-      @article.create_article_categorization!(category_id: params[:article][:category_id]) if params[:article][:category_id].present?
-      redirect_to articles_path, notice: "記事を更新しました"
-    else
-      flash.now[:danger] = "記事の更新に失敗しました"
-      render :edit
+    def create
+      @article = current_user.articles.build(article_params)
+      if @article.save
+        redirect_to articles_path, notice: "記事を作成しました"
+      else
+        flash.now[:danger] = "記事の作成に失敗しました"
+        render :new
+      end
     end
-  end
 
-  def destroy
-    @article = Article.find(params[:id])
-    if @article.destroy
-      redirect_to articles_path, notice: "記事を削除しました"
-    else
-      flash.now[:danger] = "記事の削除に失敗しました"
-      render :edit
+    def edit
+      @article = Article.find(params[:id])
+      @categories = Category.all
     end
-  end
 
-  private
-
-    def article_params
-      params.require(:article).permit(:title, :content, :slug, :image_url)
+    def update
+      @article = Article.find(params[:id])
+      if @article.update(article_params)
+        redirect_to articles_path, notice: "記事を更新しました"
+      else
+        flash.now[:danger] = "記事の更新に失敗しました"
+        render :edit
+      end
     end
+
+    def destroy
+      @article = Article.find(params[:id])
+      if @article.destroy
+        redirect_to articles_path, notice: "記事を削除しました"
+      else
+        flash.now[:danger] = "記事の削除に失敗しました"
+        render :edit
+      end
+    end
+
+    private
+
+      def article_params
+        params.require(:article).permit(:title, :content, :slug, :image_url)
+      end
+  end
 end
--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,45 +1,47 @@
-class Admin::CategoriesController < ApplicationController
-  def index
-    @categories = Category.all
-  end
-
-  def new
-    @category = Category.new
-  end
-
-  def create
-    @category = Category.new(category_params)
-    if @category.save
-      redirect_to admin_categories_path, notice: "カテゴリーを作成しました"
-    else
-      flash.now[:danger] = "カテゴリーの作成に失敗しました"
-      render :new
+module Admin
+  class CategoriesController < Admin::BaseController
+    def index
+      @categories = Category.all
     end
-  end
 
-  def edit
-    @category = Category.find(params[:id])
-  end
-
-  def update
-    @category = Category.find(params[:id])
-    if @category.update(category_params)
-      redirect_to admin_categories_path, notice: "カテゴリーを編集しました"
-    else
-      flash.now[:danger] = "カテゴリーを編集できませんでした"
-      render :edit
+    def new
+      @category = Category.new
     end
-  end
 
-  def destroy
-    @category = Category.find(params[:id])
-    @category.destroy!
-    redirect_to admin_categories_path, notice: "カテゴリー「#{@category.name}」を削除しました"
-  end
+    def create
+      @category = Category.new(category_params)
+      if @category.save
+        redirect_to admin_categories_path, notice: "カテゴリーを作成しました"
+      else
+        flash.now[:danger] = "カテゴリーの作成に失敗しました"
+        render :new
+      end
+    end
 
-  private
+    def edit
+      @category = Category.find(params[:id])
+    end
 
-    def category_params
-      params.require(:category).permit(:name)
+    def update
+      @category = Category.find(params[:id])
+      if @category.update(category_params)
+        redirect_to admin_categories_path, notice: "カテゴリーを編集しました"
+      else
+        flash.now[:danger] = "カテゴリーを編集できませんでした"
+        render :edit
+      end
+    end
+
+    def destroy
+      @category = Category.find(params[:id])
+      @category.destroy!
+      redirect_to admin_categories_path, notice: "カテゴリー「#{@category.name}」を削除しました"
+    end
+
+    private
+
+      def category_params
+        params.require(:category).permit(:name)
+      end
     end
 end
