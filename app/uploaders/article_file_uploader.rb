db27240dbb95c8165224e13a0b0a851666f6eb95
鷲見和希 <sumi@sumikazukinoMacBook-Pro.local>
2020-04-17 01:10:04 +0900

    アップロードされたファイルの保存場所変更

--- app/uploaders/article_file_uploader.rb
+++ app/uploaders/article_file_uploader.rb
@@ -10,11 +10,7 @@ class ArticleFileUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    if Rails.env.development?
-      "article_files"
-    else
-      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    end
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
