c00b6b5b47f44c68f2be5964ee6075b97806fec5
鷲見和希 <sumi@sumikazukinoMacBook-Pro.local>
2020-05-07 17:18:36 +0900

    日別のpvとcomment集計処理をdaily_article_summary_serviceに統合

---
+++ app/services/article_summaries/daily_article_summary_service.rb
@@ -0,0 +1,22 @@
+module ArticleSummaries
+  class DailyArticleSummaryService
+    # 記事毎のpv数とコメント数を日別で集計する
+    def total_pv
+      date = Date.yesterday
+      from = date.beginning_of_day
+      to = date.end_of_day
+      articles = Article.where(status: "release").where('released_at <= ?', to)
+      articles.each do |article|
+        ActiveRecord::Base.transaction do
+          summary = article.daily_article_summaries.build
+          comment = article.comments.where(created_at: from..to)
+          summary.pv_count = article.pv_count
+          summary.comment_count = comment.size
+          summary.date = date
+          summary.save!
+          article.update!(pv_count: 0)
+        end
+      end
+    end
+  end
+end
