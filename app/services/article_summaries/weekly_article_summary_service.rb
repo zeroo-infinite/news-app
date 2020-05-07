d6006ba3847f4185a6dbe0a0b8587953454cbf81
鷲見和希 <sumi@sumikazukinoMacBook-Pro.local>
2020-05-07 20:16:57 +0900

    週間のpvとcomment集計処理をweekly_article_summary_serviceに統合

--- app/services/article_summaries/weekly_article_summary_service.rb
+++ app/services/article_summaries/weekly_article_summary_service.rb
@@ -1,16 +1,21 @@
-module PvSummary
-  class WeeklyPvSummaryService
-    # 記事毎のpv数を週間で集計する
+module ArticleSummaries
+  class WeeklyArticleSummaryService
+    # 記事毎のpv数とコメント数を週間で集計する
     def total_pv
       date = Date.today
       article_ids = DailyPvSummary.where(date: date.ago(7.days)..date.yesterday).select(:article_id).distinct
       article_ids.each do |article_id|
         summaries = DailyPvSummary.where(date: date.ago(7.days)..date.yesterday).where(article_id: article_id.article_id)
-        pv = 0
-        summaries.each { |summary| pv += summary.pv_count }
+        pv_count = 0
+        comment_count = 0
+        summaries.each do |summary|
+          pv_count += summary.pv_count
+          comment_count += summary.comment_count
+        end
         WeeklyPvSummary.create!(
           article_id: article_id.article_id,
-          pv_count: pv,
+          pv_count: pv_count,
+          comment_count: comment_count,
           start_date: date.ago(7.days),
           end_date: date.yesterday
         )
