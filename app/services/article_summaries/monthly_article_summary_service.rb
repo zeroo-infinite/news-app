f1712812ffcea540e796d2f63c8a39f7eced0d81
鷲見和希 <sumi@sumikazukinoMacBook-Pro.local>
2020-05-08 16:12:41 +0900

    モデル名を修正

--- app/services/article_summaries/monthly_article_summary_service.rb
+++ app/services/article_summaries/monthly_article_summary_service.rb
@@ -3,16 +3,16 @@ module ArticleSummaries
     # 記事毎のpv数とコメント数を月間で集計する
     def summary
       date = Date.today
-      article_ids = DailyPvSummary.where(date: date.ago(1.month)..date.yesterday).select(:article_id).distinct
+      article_ids = DailyArticleSummary.where(date: date.ago(1.month)..date.yesterday).select(:article_id).distinct
       article_ids.each do |article_id|
-        summaries = DailyPvSummary.where(date: date.ago(1.month)..date.yesterday).where(article_id: article_id.article_id)
+        summaries = DailyArticleSummary.where(date: date.ago(1.month)..date.yesterday).where(article_id: article_id.article_id)
         pv_count = 0
         comment_count = 0
         summaries.each do |summary|
           pv_count += summary.pv_count
           comment_count += summary.comment_count
         end
-        MonthlyPvSummary.create!(
+        MonthlyArticleSummary.create!(
           article_id: article_id.article_id,
           pv_count: pv_count,
           comment_count: comment_count,
--- app/services/article_summaries/weekly_article_summary_service.rb
+++ app/services/article_summaries/weekly_article_summary_service.rb
@@ -3,16 +3,16 @@ module ArticleSummaries
     # 記事毎のpv数とコメント数を週間で集計する
     def summary
       date = Date.today
-      article_ids = DailyPvSummary.where(date: date.ago(7.days)..date.yesterday).select(:article_id).distinct
+      article_ids = DailyArticleSummary.where(date: date.ago(7.days)..date.yesterday).select(:article_id).distinct
       article_ids.each do |article_id|
-        summaries = DailyPvSummary.where(date: date.ago(7.days)..date.yesterday).where(article_id: article_id.article_id)
+        summaries = DailyArticleSummary.where(date: date.ago(7.days)..date.yesterday).where(article_id: article_id.article_id)
         pv_count = 0
         comment_count = 0
         summaries.each do |summary|
           pv_count += summary.pv_count
           comment_count += summary.comment_count
         end
-        WeeklyPvSummary.create!(
+        WeeklyArticleSummary.create!(
           article_id: article_id.article_id,
           pv_count: pv_count,
           comment_count: comment_count,
